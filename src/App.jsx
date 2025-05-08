287a5d7138369951ef347f3991ad4c1b4ae28158
Goga-Chkhaidze <chxaidze.goga@list.ru>
2025-05-08 12:42:41 -0700

    sada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/footer';
 import AboutUs from './pages/aboutus';
 import Home from './homepage';
 import ContactUs from './pages/ContactUs.jsx';
